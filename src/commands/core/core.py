63d01eaff6910916f9fb2938f2aa344b24ec2412
kebabulon <3dmod07@mail.ru>
2025-10-09 19:02:40 +0300

    Make `-l` flag in `ls()` print a nicely formatted table

--- src/commands/core/core.py
+++ src/commands/core/core.py
@@ -1,8 +1,24 @@
 import os
+import stat
+from pwd import getpwuid
+from grp import getgrgid
+
 from datetime import datetime
+from typing import NamedTuple
 
 from argparse import ArgumentParser
 from src.command import command, CommandEnv
+
+from src.path import pretty_path
+
+
+class TableRow(NamedTuple):
+    permissions: str
+    user: str
+    group: str
+    size: str
+    modification_time: str
+    name: str
 
 
 @command(
@@ -21,18 +37,33 @@ def ls(env: CommandEnv, args: list[str]) -> None:
     dir_path = env.get_path(argv.dir)
 
     if not os.path.isdir(dir_path):
-        raise FileNotFoundError(f"No such file or dir {dir_path}")
-
-    # TODO: check if path is valid before listing
-    listdir_itterator = os.listdir(dir_path)
+        raise FileNotFoundError(f"No such file or dir {pretty_path(dir_path)}")
 
     if not argv.l:
-        env.print(" ".join(listdir_itterator))
+        env.print(" ".join(os.listdir(dir_path)))
     else:
-        for file in listdir_itterator:
-            # TODO: this does not follow symlinks... probably good?
-            file_path = os.path.join(dir_path, file)
-            file_stat = os.stat(file_path)
-            modification_time = datetime.utcfromtimestamp(file_stat.st_mtime).strftime('%Y-%m-%d %H:%M:%S')
-            # TODO: create array, then create a table string out of it with proper spacing
-            env.print(f"{file_stat.st_size} {modification_time} {file}")
+        table: list[TableRow] = []
+
+        max_user_len = 0
+        max_group_len = 0
+        max_size_len = 0
+
+        for file in os.scandir(dir_path):
+            file_stat = file.stat(follow_symlinks=True)
+
+            row = TableRow(
+                permissions=stat.filemode(file_stat.st_mode),
+                user=getpwuid(file_stat.st_uid).pw_name,
+                group=getgrgid(file_stat.st_gid).gr_name,
+                size=str(file_stat.st_size),
+                modification_time=datetime.utcfromtimestamp(file_stat.st_mtime).strftime('%Y-%m-%d %H:%M:%S'),
+                name=file.name,
+            )
+            table.append(row)
+
+            max_user_len = max(max_user_len, len(row.user))
+            max_group_len = max(max_group_len, len(row.group))
+            max_size_len = max(max_size_len, len(row.size))
+
+        for row in table:
+            env.print(f"{row.permissions} {row.user:<{max_user_len}} {row.group:<{max_group_len}} {row.size:<{max_size_len}} {row.modification_time} {row.name}")
