d00657ad958a104428da32bdf1cc781485c80c69
kebabulon <3dmod07@mail.ru>
2025-10-10 21:30:49 +0300

    Remove `pretty_path()` from `validate_path()` error message

--- src/path.py
+++ src/path.py
@@ -8,4 +8,4 @@ def pretty_path(path: str) -> str:
 
 def validate_path(path: str) -> None:
     if not os.path.exists(path):
-        raise FileNotFoundError(f"No such file or dir {pretty_path(path)}")
+        raise FileNotFoundError(f"No such file or dir {path}")
