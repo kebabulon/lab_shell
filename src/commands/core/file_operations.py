bda13169ce4a57ab239f4680c31d2f92f20430f5
kebabulon <3dmod07@mail.ru>
2025-10-11 13:39:42 +0300

    Add `cp` command and raise an exception when moving directory into a file

--- src/commands/core/file_operations.py
+++ src/commands/core/file_operations.py
@@ -7,6 +7,52 @@ from src.path import validate_path
 
 
 @command(
+    name="cp",
+    description="copy file/directory",
+    help="""
+        source - file/directory to copy
+        dest - destination to copy file/directory to
+
+        -r - recursively copy the contents of the directory
+    """
+)
+def cmd_cp(env: CommandEnv, args: list[str]) -> None:
+    parser = ArgumentParser(exit_on_error=False)
+    parser.add_argument('source')
+    parser.add_argument('dest')
+    parser.add_argument('-r', action='store_true')
+    argv = parser.parse_args(args)
+
+    source_path = env.get_path(argv.source)
+    validate_path(source_path)
+
+    dest_path = env.get_path(argv.dest)
+
+    log_dest_path = dest_path
+    if os.path.isdir(dest_path):
+        log_dest_path = os.path.join(dest_path, os.path.basename(source_path))
+
+    if os.path.isfile(source_path):
+        try:
+            shutil.copy2(source_path, dest_path)
+        except PermissionError:
+            raise PermissionError("No permission")
+
+    elif os.path.isdir(source_path):
+        if not argv.r:
+            raise IsADirectoryError("Unable to copy directory without '-r' flag present")
+
+        try:
+            shutil.copytree(source_path, dest_path)
+        except PermissionError:
+            raise PermissionError("No permission")
+        except FileExistsError:
+            raise FileExistsError(f"Destination is a file {dest_path}")
+
+    env.log_success(f"Successfully copied {source_path} to {log_dest_path}")
+
+
+@command(
     name="mv",
     description="move/rename file/directory",
     help="""
@@ -33,5 +79,7 @@ def cmd_mv(env: CommandEnv, args: list[str]) -> None:
         shutil.move(source_path, dest_path)
     except PermissionError:
         raise PermissionError("No permission")
+    except FileExistsError:
+        raise FileExistsError(f"Destination is a file {dest_path}")
 
     env.log_success(f"Successfully moved {source_path} to {log_dest_path}")
