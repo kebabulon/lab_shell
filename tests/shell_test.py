8b50d187faf72867dc9bee46ed312468da01d250
kebabulon <3dmod07@mail.ru>
2025-10-26 19:19:03 +0300

    Add `shell` test

---
+++ tests/shell_test.py
@@ -0,0 +1,9 @@
+import pytest
+
+from tests.setup import sandbox_shell
+
+
+def test_shell(sandbox_shell):
+    #  command not found
+    with pytest.raises(NameError):
+        sandbox_shell.execute("test")
